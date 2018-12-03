26b3afc13650f2487dbec852fb893dc18e6dec1c
Raoul Rubien <rubienr@gmail.com>
2018-12-03 19:18:17 +0000

    migrated file from 3.x to 4.x kernel sysfs entry

--- machinekit/configs/ARM.BeagleBone.Panther/setup.bridge.sh
+++ machinekit/configs/ARM.BeagleBone.Panther/setup.bridge.sh
@@ -16,44 +16,57 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
 
-dtbo_err () {
-	echo "Error loading device tree overlay file: $DTBO" >&2
-	exit 1
+function dtbo_err ()
+{
+  echo "Error loading device tree overlay file: $1" >&2
+  exit 1
 }
 
-pin_err () {
-	echo "Error exporting pin:$PIN" >&2
-	exit 1
+function pin_err () {
+  echo "Error exporting pin:$1" >&2
+  exit 1
 }
 
-dir_err () {
-	echo "Error setting direction:$DIR on pin:$PIN" >&2
-	exit 1
+function dir_err () {
+  local dir=$1
+  local pin=$2
+  echo "Error setting direction:$1 on pin:$2" >&2
+  exit 1
 }
 
-SLOTS=/sys/devices/bone_capemgr.*/slots
+function try_load_overlays()
+{
+  local slots=/sys/devices/platform/bone_capemgr/slots
 
-# Make sure required device tree overlay(s) are loaded
-for DTBO in panther cape-bone-iio ; do
+  # Make sure required device tree overlay(s) are loaded
+  for dtbo in panther cape-bone-iio ; do
 
-	if grep -q $DTBO $SLOTS ; then
-		echo $DTBO overlay found
-	else
-		echo Loading $DTBO overlay
-		sudo -A su -c "echo $DTBO > $SLOTS" || dtbo_err
-		sleep 1
-	fi
-done;
+    if grep -q $dtbo $slots ; then
+      echo $dtbo overlay found
+    else
+      echo Loading $dtbo overlay
+      sudo -A su -c "echo $dtbo > $slots" || dtbo_err $dtbo
+      sleep 1
+    fi
+  done;
+}
 
-if [ ! -r /sys/devices/ocp.*/helper.*/AIN0 ] ; then
-	echo Analog input files not found in /sys/devices/ocp.*/helper.* >&2
-	exit 1;
-fi
+function assert_sys_files_exist()
+{
+  # see bbb debian migration: https://elinux.org/Beagleboard:BeagleBone_Debian_Image_Migration
 
-if [ ! -r /sys/class/uio/uio0 ] ; then
-	echo PRU control files not found in /sys/class/uio/uio0 >&2
-	exit 1;
-fi
+  local ain_helper=/sys/bus/iio/devices/iio:device0
+  if [ ! -r $ain_helper ] ; then
+    echo "Analog input files not found in $ain_helper" >&2
+    exit 1;
+  fi
+
+  local pru_control=/sys/devices/platform/ocp/*.pruss/uio/uio0
+  if [ ! -r $pru_control ] ; then
+    echo "PRU control files not found in $pru_control" >&2
+    exit 1;
+  fi
+}
 
 # Export GPIO pins:
 # One pin needs to be exported to enable the low-level clocks for the GPIO
@@ -73,49 +86,69 @@ fi
 # causing problems.  You may wish to do this for documentation or to make
 # sure the pin starts with a known value as soon as possible.
 
-while read PIN DIR JUNK ; do
-        case "$PIN" in
-        ""|\#*)	
-		continue ;;
-        *)
-		[ -r /sys/class/gpio/gpio$PIN ] && continue
-                sudo -A su -c "echo $PIN > /sys/class/gpio/export" || pin_err
-		sudo -A su -c "echo $DIR > /sys/class/gpio/gpio$PIN/direction" || dir_err
-                ;;
-        esac
+function gonfigure_gpio_pins()
+{
+  while read pin_number direction delimiter pin_name gpio_name ; do
+    case "$pin_number" in
+      ""|\#*)
+        continue ;;
+      *)
 
-done <<- EOF
-	30	in	# p9.11		gpio0.30
-	31	in	# p9.13		gpio0.31
-	48	in	# p9.15		gpio1.16
-	27	in	# P8.17		gpio0.27
-	49	in	# P9.23		gpio1.17
-	117	in	# P9.25		gpio3.21
-	115	in	# P9.27		gpio3.19
-	14	in	# P9.26		gpio0.14
+        local gpio_pin=/sys/class/gpio/gpio$pin_number
+        if [ -r $gpio_pin ] ; then
+          continue
+        fi
 
-	15	in	# P9.24		gpio0.15
-	61	in	# P8.26		gpio1.29
-	60	in	# P9.12		gpio1.28
-	47	in	# P8.15		gpio1.15
-	7	in	# P9.42		gpio0.7
-	65	in	# P8.18		gpio2.1
-	2	in	# P9.22		gpio0.2
-	3	in	# P9.21		gpio0.3
+        echo "pin_number=$pin_number direction=$direction" \
+        "(pin_name=$pin_name gpio_name=$gpio_name)"
+        sudo -A su -c "echo $pin_number > /sys/class/gpio/export" || \
+        pin_err $pin_number
+        sudo -A su -c "echo $direction > ${gpio_pin}/direction" || \
+        dir_err $direction $pin_number
+      ;;
+    esac
 
-	69	out	# P8.9		gpio2.5
-	66	out	# P8.7		gpio2.2
-	67	out	# P8.8		gpio2.3
-	68	out	# P8.10		gpio2.4
-	44	out	# P8.12		gpio1.12
-	45	out	# P8.11		gpio1.13
-	26	out	# P8.14		gpio0.26
-	46	out	# P8.16		gpio1.14
+  done <<- EOF
+30  in  # p9.11 gpio0.30
+31  in  # p9.13 gpio0.31
+48  in  # p9.15 gpio1.16
+27  in  # P8.17 gpio0.27
+49  in  # P9.23 gpio1.17
+117 in  # P9.25 gpio3.21
+115 in  # P9.27 gpio3.19
+14  in  # P9.26 gpio0.14
 
-	23	out	# P8.13		gpio0.23
-	22	out	# P8.19		gpio0.22
-	50	out	# P9.14		gpio1.18
-	51	out	# P9.16		gpio1.19
-	5	out	# p9.17		gpio0.5
-	4	out	# p9.18		gpio0.4
+15  in  # P9.24  gpio0.15
+61  in  # P8.26  gpio1.29
+60  in  # P9.12  gpio1.28
+47  in  # P8.15  gpio1.15
+7   in  # P9.42  gpio0.7
+65  in  # P8.18  gpio2.1
+2   in  # P9.22  gpio0.2
+3   in  # P9.21  gpio0.3
+
+69  out # P8.9  gpio2.5
+66  out # P8.7  gpio2.2
+67  out # P8.8  gpio2.3
+68  out # P8.10 gpio2.4
+44  out # P8.12 gpio1.12
+45  out # P8.11 gpio1.13
+26  out # P8.14 gpio0.26
+46  out # P8.16 gpio1.14
+
+23  out # P8.13 gpio0.23
+22  out # P8.19 gpio0.22
+50  out # P9.14 gpio1.18
+51  out # P9.16 gpio1.19
+5   out # p9.17 gpio0.5
+4   out # p9.18 gpio0.4
 EOF
+}
+
+function main()
+{
+  try_load_overlays
+  assert_sys_files_exist
+  gonfigure_gpio_pins
+}
+main $@
